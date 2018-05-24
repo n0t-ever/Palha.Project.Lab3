c4234aa1655fd9ba922323ad2498b0924f759dc8
n0t-ever <37885776+n0t-ever@users.noreply.github.com>
2018-05-23 21:27:54 -0300

    Só os paranoicos Salvam...
    Convertendo em dialog... 50% terminado

--- WebServerApp/scripts/ScriptApache.bash
+++ WebServerApp/scripts/ScriptApache.bash
@@ -4,24 +4,30 @@
 # -----------------------------------------------------------------------------------------  #
 # Este Programa recebe o nome do site e do dominio, será criado a pasta e atribuido o usuário#
 # www-data como dono. Os arquivos do site não será criado.
-
+#
+## Atualizações
+# 23/05/2018 - Agora com interface mais amigavel
+#
 #  Licença: LGPL v3 (GNU Lesser General Public License v3.0)								 #
 #  #######################################################################################   #
-
-echo -n "Entre com o Nome do site: " ; read SiteConf
-echo -n "Entre com o dominio (.org, .com)" ; read DomSite
-
+# Vamos pegar algumas informações com o usuario.
+SiteConf=$( dialog --stdout --inputbox 'Entre com o Nome do site: '
+DomSite=$( dialog --stdout --inputbox 'Entre com o dominio' 0 0 "(.org, .com)"
+WebAdmin=$( dialog --stdout --inputbox 'Email do Web Admin:' 0 0
+if [ -z "${WebAdmin}" ]; then
+    dialog --title 'Email' --msgbox 'O email está vazio, será incluido um teste@teste.com' 6 40
+fi  
+# Define o caminho do sitio
 PathSite="/var/www/${SiteConf}"
+# A pasta será criada e o usuario www-data será o dono.
 mkdir $PathSite ; chmod 755 $PathSite; chown -R "www-data:www-data" $PathSite 
+# Criar o arquivo com as configurações do virtual hosts
 touch /etc/apache2/sites-available/$SiteConf.conf
+# Atribuir permisão para os arquivos
 chmod 755 '/etc/apache2/sites-available/'$SiteConf.conf
 
+# Vamos inserias as arquivo
 echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$SiteConf.conf
-
-echo -n "Email do Web Admin: " ; read WebAdmin
-if [ -z "${WebAdmin}" ]; then
-    echo "O email está vazio, será incluido um teste@teste.com"
-fi  
 
 echo "ServerAdmin ${WebAdmin:-teste@teste.com} " >> /etc/apache2/sites-available/$SiteConf.conf
 echo "ServerName ${SiteConf}" >>  /etc/apache2/sites-available/$SiteConf.conf
@@ -35,5 +41,8 @@ echo "www.${SiteConf}${DomSite}" >> /etc/hosts
 # #Vamos mexer mesmo no DNS?
 # #echo $SiteConf + " IN 	A 	192.168.34.55"	>> /etc/bind/zones/db.Replicante.org
 
-a2ensite $SiteConf.conf ; service apache2 restart
+a2ensite $SiteConf.conf
+#Reiniciar o serviço
+service apache2 stop && service apache2 start
+#Voltamos a pancheta de lançamento.
 bash /srv/Projeto.Palha/WebServerApp/MenuUser.bash
